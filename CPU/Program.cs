19f8e3943f8c94cbd30632130e71246942e24ade
jaycdave88 <jaycdave@gmail.com>
2016-03-01 11:21:48 -0600

    first commit - adding quick tool code for monitoring cpu

---
+++ CPU/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace CPU
+{
+    class Program
+    {
+        private static string appName;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a Application to monitor");
+            appName = Console.ReadLine();
+
+            PerformanceCounter myAppCPU =
+             new PerformanceCounter("Process", "% Processor Time", appName, true);
+
+            Console.WriteLine("Press the any key to stop ... \n");
+
+            if (myAppCPU != null)
+            {
+                while (!Console.KeyAvailable)
+                {
+                    double pct = myAppCPU.NextValue();
+                    Console.WriteLine("CPU % = " + pct);
+                    Thread.Sleep(2500);
+                }
+            }
+            else
+                Console.WriteLine("No Process found");
+        }
+    }
+}
